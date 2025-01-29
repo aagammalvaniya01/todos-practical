79c4746e491dc9e1b2bb1604a43c5c26fd91e13f
Aagam <aagammalvaniya01@gmail.com>
2025-01-29 23:09:12 +0530

    [feat] build issue fix

---
+++ .next/static/chunks/app/tasks/page-7f9719a5466142cd.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[147],{6418:(e,s,r)=>{Promise.resolve().then(r.bind(r,1281))},1281:(e,s,r)=>{"use strict";r.d(s,{default:()=>d});var a=r(5155);r(2115);let d=()=>(0,a.jsx)("div",{children:"DashBoardWrapper"})}},e=>{var s=s=>e(e.s=s);e.O(0,[441,517,358],()=>s(6418)),_N_E=e.O()}]);
